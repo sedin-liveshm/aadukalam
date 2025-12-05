8791a8b473b17712f1872d4ce2036778e8b7371f
sedin-liveshm <livesh.m@sedintechnologies.com>
2025-12-05 15:50:46 +0530

    Fix

--- student/src/pages/ReviewQuestionPage/ReviewQuestionPage.jsx
+++ student/src/pages/ReviewQuestionPage/ReviewQuestionPage.jsx
@@ -4,18 +4,18 @@ import { useNavigate, useParams } from "react-router-dom";
 import { API_URL } from '../../config';
 function ReviewQuestionPage(){
     const {uname , qname } = useParams()
-    const {reviewData , setReviewData} = useState({})
+    const [reviewData , setReviewData] = useState({})
     useEffect(()=>{
         const getData = async() =>{
-            const review = await fetch(`${API_URL}/basic/review-question",{
+            const review = await fetch(`${API_URL}/basic/review-question`, {
                 method:"POST",
-                body: JSON.stringify(loginData),
+                body: JSON.stringify({uname, qname}),
                 headers:{
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
             })
-            const data = await otp.json()
+            const data = await review.json()
             setReviewData(data)
         }
 
